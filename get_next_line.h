a4967cb1ed8e7a76d695a7ca8aaf00c2bc2d3d03
maxigit42 <maximowilliner34@gmail.com>
2024-11-14 13:33:50 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilline <mwilline@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 18:20:33 by mwilline          #+#    #+#             */
+/*   Updated: 2024/11/12 13:42:38 by mwilline         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
